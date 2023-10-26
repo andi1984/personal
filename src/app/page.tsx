0dd5d1679aec025a4bc51cb1f7d023721d4b4a86
Andreas Sander <andreas.sander@hotmail.de>
2023-10-26 23:42:15 +0200

    Adding followercount

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from 'next/image'
+async function getData(): Promise<{ total: number }> {
+  if (!process.env.FOLLOWER_API_ENDPOINT) {
+    return { total: 0 };
+  }
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello world</h1>
-    </main>
-  )
+  const res = await fetch(process.env.FOLLOWER_API_ENDPOINT);
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
+
+const Page = async ({}) => {
+  const res = await getData();
+  return <h1>Counter: {res.total} Followers</h1>;
+};
+
+export default Page;
