34c87b1a031fe2fd3a21d762ad9d0ab4c32bf071
Andreas Sander <mail@andi1984.de>
2023-11-04 22:56:28 +0100

    change cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,26 @@ const Page = async ({}) => {
   const api = await import("./api/follower/route");
   const response = await api.GET();
   const data = await response.json();
-  const posts = getAllPosts(["slug", "title"]);
+  const posts = getAllPosts(["slug", "title", "description"]);
   return (
     <>
       <MastHead count={data.total} />
       <nav>
-        <ol>
+        <ol className="grid grid-cols-4 gap-4">
           {posts.map((post) => (
             <li key={post.slug}>
-              <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+              <Link href={`/posts/${post.slug}`} className="">
+                <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2 inline-flex items-center text-center text-black">
+                      {post.title}
+                    </div>
+                    <p className="text-gray-700 text-base">
+                      {post.description}
+                    </p>
+                  </div>
+                </div>
+              </Link>
             </li>
           ))}
         </ol>
