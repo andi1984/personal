b4d120467ae3ebed5a858347b0cc8f7f1e878e0d
Andreas Sander <mail@andi1984.de>
2023-11-04 20:29:27 +0100

    feat: landing page basic design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import MastHead from "@/components/masthead";
+
 const Page = async ({}) => {
   const api = await import("./api/follower/route");
   const response = await api.GET();
   const data = await response.json();
-  return <h1>Counter: {data.total} Followers</h1>;
+  return (
+    <>
+      <MastHead count={data.total} />
+    </>
+  );
 };
 
 export default Page;
