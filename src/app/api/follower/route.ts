c349ecd9f87af8cdbfc7a50a96b307505399029a
Andreas Sander <mail@andi1984.de>
2023-11-04 19:24:29 +0100

    chore: Add backend API endpoint

---
+++ src/app/api/follower/route.ts
@@ -0,0 +1,20 @@
+type API_RESPONSE = { total: number };
+export async function GET() {
+  if (!process.env.FOLLOWER_API_ENDPOINT) {
+    return Response.json({ total: 0 });
+  }
+
+  const res = await fetch(process.env.FOLLOWER_API_ENDPOINT, {
+    next: { revalidate: 60 * 60 }, // Revalidate every 60 seconds
+  });
+
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  const data = (await res.json()) as API_RESPONSE;
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,8 @@
-async function getData(): Promise<{ total: number }> {
-  if (!process.env.FOLLOWER_API_ENDPOINT) {
-    return { total: 0 };
-  }
-
-  const res = await fetch(process.env.FOLLOWER_API_ENDPOINT);
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
 const Page = async ({}) => {
-  const res = await getData();
-  return <h1>Counter: {res.total} Followers</h1>;
+  const api = await import("./api/follower/route");
+  const response = await api.GET();
+  const data = await response.json();
+  return <h1>Counter: {data.total} Followers</h1>;
 };
 
 export default Page;
