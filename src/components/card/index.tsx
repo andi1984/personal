20850e5b4f2fe012971e2af8e32c5b7e717bb12e
Andreas Sander <mail@andi1984.de>
2023-11-16 22:20:24 +0100

    Update card layout

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -2,6 +2,14 @@ import React, { FC } from "react";
 import Link from "next/link";
 import { Items, Types } from "@/lib/types";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const BlogPostCard: FC<{ post: Items; type: Types }> = ({ post, type }) => {
   const { slug, title, date, description } = post;
   // Formats the date string to a more readable format.
@@ -10,18 +18,24 @@ const BlogPostCard: FC<{ post: Items; type: Types }> = ({ post, type }) => {
   };
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-6">
-      <small className="block text-gray-700 text-base">
-        <time dateTime={date}>{formatDate(date)}</time>
-      </small>
-      <Link
-        className="font-bold text-xl mb-2 transition duration-300"
-        href={`/${type}s/${slug}`}
-      >
-        {title}
-      </Link>
-      <p>{description}</p>
-    </div>
+    <Card className="">
+      <CardHeader>
+        <CardTitle>
+          <Link
+            className="font-bold text-xl mb-2 transition duration-300"
+            href={`/${type}s/${slug}`}
+          >
+            {title}
+          </Link>
+        </CardTitle>
+        <CardDescription>
+          <small>
+            <time dateTime={date}>{formatDate(date)}</time>
+          </small>
+        </CardDescription>
+      </CardHeader>
+      {description && <CardContent>{description}</CardContent>}
+    </Card>
   );
 };
 
