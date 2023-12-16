70f11413a9bf9e40b419571292a5ed4d6ef6f5d1
Andreas Sander <mail@andi1984.de>
2023-12-16 20:20:56 +0100

    feat: add ids to headings

--- src/lib/markdown_to_html.ts
+++ src/lib/markdown_to_html.ts
@@ -2,12 +2,16 @@ import rehypeExternalLinks from "rehype-external-links";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { unified } from "unified";
 
 export default async function markdownToHtml(markdown: string) {
   const processor = await unified()
     .use(remarkParse)
     .use(remarkRehype)
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings)
     .use(rehypeExternalLinks, { rel: ["nofollow"], target: "_blank" })
     .use(rehypeStringify);
 
