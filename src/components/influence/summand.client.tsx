9ed8bd747c9324021fef2448671c94bc31f669ee
Andreas Sander <mail@andi1984.de>
2024-01-20 00:49:39 +0100

    some icons

--- src/components/influence/summand.client.tsx
+++ src/components/influence/summand.client.tsx
@@ -1,27 +1,35 @@
 import Link from "next/link";
 import { FC } from "react";
 
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-
+import { TableCell, TableRow } from "@/components/ui/table";
+import { FaMastodon, FaYoutube, FaDev, FaGithub } from "react-icons/fa";
+import { IoShareSocialOutline } from "react-icons/io5";
 import Number from "./number.client";
 import { Button } from "../ui/button";
 
+const getSoMeIcon = (platform: string) => {
+  switch (platform.toLowerCase()) {
+    case "mastodon":
+      return FaMastodon;
+    case "youtube":
+      return FaYoutube;
+    case "dev.to":
+      return FaDev;
+    case "github":
+      return FaGithub;
+    default:
+      return IoShareSocialOutline;
+  }
+};
 const Summand: FC<{
   platform: string;
   count: number;
   url?: string;
   className?: string;
 }> = ({ className, platform, count, url }) => {
+  const Icon = getSoMeIcon(platform);
   return (
-    <TableRow className={`${className} `}>
+    <TableRow className={`${className}`}>
       <TableCell>
         <Number n={count} />
       </TableCell>
@@ -29,6 +37,7 @@ const Summand: FC<{
         {url ? (
           <Button className="p-0 underline" asChild variant="link">
             <Link href={url} target="_blank">
+              <Icon className="mr-1 fill-current" />
               {platform}
             </Link>
           </Button>
