77611d7544616b707e2130eec0d8ca45fcdbb1c4
Andreas Sander <mail@andi1984.de>
2024-09-22 19:03:48 +0200

    tabstructure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import AllPostsList from "@/components/list/posts";
 import { getAllPosts } from "@/lib/get_all_posts";
 import SocialMediaInfluence from "@/components/influence";
+import * as Tabs from "@radix-ui/react-tabs";
 import MastHead from "@/components/masthead";
 import Link from "next/link";
 
@@ -32,64 +33,33 @@ const Page = async ({}) => {
             between nurturing plants and developing robust websites.
           </p>
         </section>
-
-        <section>
-          <h3>My Journey in Web Development and Gardening</h3>
-          <ul>
-            <li>
-              <strong>React &amp; JavaScript Projects:</strong> Check out how I
-              apply precision and creativity, much like in gardening, to my
-              React and JavaScript projects.
-            </li>
-            <li>
-              <strong>TypeScript and CSS:</strong> Discover the structured
-              beauty in my code, reminiscent of a well-kept garden, thanks to
-              TypeScript and modern CSS techniques.
-            </li>
-            <li>
-              <strong>Insights and Growth:</strong> Just like in gardening, I
-              believe in constant learning and growth in web development. Dive
-              into my blog for thoughts, tips, and shared knowledge.
-            </li>
-          </ul>
-        </section>
-
-        <section>
-          <h3>Connecting Online and in the Garden</h3>
-          <p>
-            While you&apos;ll find me coding and sharing knowledge across
-            various online platforms, I also take time to step away from the
-            screen and connect with nature in my garden. This balance keeps me
-            creative and inspired:
-          </p>
-          <SocialMediaInfluence count={data} />
-        </section>
-
-        <section>
-          <h3>Beyond the Code: Community, Nature, and Creativity</h3>
-          <p>
-            I love blending my coding skills with my passion for gardening.
-            It&apos;s not just about nurturing plants; it&apos;s about nurturing
-            ideas and relationships, whether it&apos;s through mentoring budding
-            developers or collaborating on innovative projects.
-          </p>
-        </section>
-
-        <section>
-          <h3>Let&apos;s Grow Together!</h3>
-          <p>
-            Whether you&apos;re into tech, gardening, or both, I&apos;m eager to
-            connect. Explore my site, follow me on my various channels, and
-            let&apos;s share our journeys in both web development and the
-            natural world.
-          </p>
-        </section>
-        <section id="posts">
-          <AllPostsList title="All posts" posts={posts} type="post" />
-        </section>
-        <section id="notes">
-          <AllPostsList title="All notes" posts={notes} type="note" />
-        </section>
+        <Tabs.Root className="TabsRoot" defaultValue="posts">
+          <Tabs.List className="TabsList">
+            <Tabs.Trigger value="posts" className="TabsTrigger">
+              Blogposts
+            </Tabs.Trigger>
+            <Tabs.Trigger value="notes" className="TabsTrigger">
+              Notes
+            </Tabs.Trigger>
+            <Tabs.Trigger value="community" className="TabsTrigger">
+              Community
+            </Tabs.Trigger>
+          </Tabs.List>
+          <Tabs.Content value="posts" className="TabsContent">
+            <section id="posts">
+              <AllPostsList title="All posts" posts={posts} type="post" />
+            </section>
+          </Tabs.Content>
+          <Tabs.Content value="notes" className="TabsContent">
+            <section id="notes">
+              <AllPostsList title="All notes" posts={notes} type="note" />
+            </section>
+          </Tabs.Content>
+          <Tabs.Content value="community" className="TabsContent">
+            <p>Please contact me on one of those platforms listed below:</p>
+            <SocialMediaInfluence count={data} />
+          </Tabs.Content>
+        </Tabs.Root>
       </main>
       <footer>
         <Link href="/impressum">Imprint & Privacy statement</Link>
