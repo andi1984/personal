ebda3f5966518334c120b1eba6fbf5468c0243b9
Andreas Sander <mail@andi1984.de>
2025-02-23 11:41:58 +0100

    remove non-react19 supporting deps

--- src/components/influence/number.client.tsx
+++ src/components/influence/number.client.tsx
@@ -1,15 +1,7 @@
 "use client";
-import { useSpring, animated } from "@react-spring/web";
 
 function Number({ className, n }: { className?: string; n: number }) {
-  const { number } = useSpring({
-    from: { number: 0 },
-    to: { number: n },
-    delay: 200,
-    config: { mass: 1, tension: 20, friction: 10 },
-  });
-
-  return <animated.span>{number.to((num) => num.toFixed(0))}</animated.span>;
+  return <span className={className}>{n}</span>;
 }
 
 export default Number;
