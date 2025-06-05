989ba9d1617d64d9e416c5a30aac1a1c01993dbd
Andreas Sander <mail@andi1984.de>
2025-06-05 21:32:49 +0200

    Improve landing page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,18 +23,16 @@ const Page = async ({}) => {
         Skip to Main Content
       </a>
 
-      <main>
+      <main className="max-w-5xl mx-auto px-4 space-y-8">
         <MastHead />
-        <section>
-          <h2>Welcome to My Space: Where Web Development Meets Gardening</h2>
-          <p>
-            Hello and thanks for visiting! I&apos;m Andi, a dedicated senior
-            frontend developer from the beautiful South-West of Germany.
-            I&apos;m all about creating smooth and engaging web experiences with
-            React, JavaScript, TypeScript, and CSS. But there&apos;s more to me
-            than just code – I&apos;m also an avid gardener, finding parallels
-            between nurturing plants and developing robust websites.
-          </p>
+        <section className="relative overflow-hidden rounded-3xl bg-gradient-to-br from-emerald-500 via-emerald-400 to-indigo-500 dark:from-emerald-800 dark:via-emerald-600 dark:to-indigo-800 text-background p-8 shadow-lg">
+          <div className="absolute inset-0 bg-[url('/header.png')] bg-cover bg-center opacity-20"></div>
+          <div className="relative z-10 space-y-4 text-center">
+            <h2 className="text-2xl md:text-3xl font-bold">Welcome to My Space: Where Web Development Meets Gardening</h2>
+            <p className="text-lg">
+              Hello and thanks for visiting! I&apos;m Andi, a dedicated senior frontend developer from the beautiful South-West of Germany. I&apos;m all about creating smooth and engaging web experiences with React, JavaScript, TypeScript, and CSS. But there&apos;s more to me than just code – I&apos;m also an avid gardener, finding parallels between nurturing plants and developing robust websites.
+            </p>
+          </div>
         </section>
         <Tabs.Root className="TabsRoot" defaultValue="posts">
           <Tabs.List className="TabsList">
